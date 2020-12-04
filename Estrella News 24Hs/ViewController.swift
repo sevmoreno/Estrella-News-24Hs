84c42cfd0382293f5480680dae8b7b23eae5d0e2
Sebastian Moreno Breser <sevmoreno@gmail.com>
2020-12-03 23:37:01 -0600

    Initial Commit

---
+++ Estrella News 24Hs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Estrella News 24Hs
+//
+//  Created by Juan Moreno on 12/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
